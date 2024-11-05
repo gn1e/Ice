0bd88eba45a79ac12fff80870a2f71577d75bdb8
nade <88284830+gn1e@users.noreply.github.com>
2024-11-05 12:30:59 +0200

    Update waitingroom.js

--- routes/waitingroom.js
+++ routes/waitingroom.js
@@ -1,6 +1,8 @@
 const web = require("express");
 const app = web.Router();
 
+// why did i do a seprate file for this
+
 app.get("/waitingroom/api/waitingroom", (req, res) => res.sendStatus(204));
 
 module.exports = app;
