4345d247f989d8abdf56e20bde6f1c29ccb1dc2e
teko <discord.xys@gmail.com>
2024-11-06 11:48:49 -0700

    news api

--- Api/routes/news.js
+++ Api/routes/news.js
@@ -1,1 +1,24 @@
 
+app.get('/api/launcher/news/public', (req, res) => {
+
+  const jsonResponse = {
+    status: "success",
+    data: [
+      {
+        id: 1,
+        title: "launcher news",
+        description: "describe your news and updates here",
+        date: "2024-11-06",
+      },
+      {
+        id: 2,
+        title: "same here",
+        description: "lowkey good for launcher",
+        date: "2024-11-13",
+      },
+    ],
+  };
+
+
+  res.json(jsonResponse);
+});
