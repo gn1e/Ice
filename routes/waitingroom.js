233435f8f805245d669cf1a28095cde6476395b9
nade <88284830+gn1e@users.noreply.github.com>
2024-11-06 16:33:30 +0200

    waitingroom que :soon:

--- routes/waitingroom.js
+++ routes/waitingroom.js
@@ -1,8 +1,34 @@
-const web = require("express");
-const app = web.Router();
+const express = require("express");
+const app = express.Router();
 
-// why did i do a seprate file for this
+const maxonline = 50;
+const quetime = 5 * 60 * 1000; 
 
-app.get("/waitingroom/api/waitingroom", (req, res) => res.sendStatus(204));
+let playersinque = [];
+
+function getOnlinePlayersCount() {
+    // one day
+    return 0; 
+}
+
+// this is probably not proper
+function yesyes() {
+    if (playersinque.length > 0) {
+        const { req, res } = playersinque.shift(); 
+        res.sendStatus(204); 
+    }
+}
+
+setInterval(yesyes, quetime);
+
+app.get("/waitingroom/api/waitingroom", (req, res) => {
+    const playeronline = getOnlinePlayersCount();
+
+    if (playeronline >= maxonline) {
+        playersinque.push({ req, res });
+    } else {
+        res.sendStatus(204);
+    }
+});
 
 module.exports = app;
